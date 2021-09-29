f0e1b80f7e7b019477ed11c9b8d8926b6a71fb5b
GregoryAyon <royayon229@gmail.com>
2021-09-29 20:39:05 +0600

    PySimpleGui File Updated

--- simple_Add_UI.py
+++ simple_Add_UI.py
@@ -6,11 +6,11 @@ def addFunc(num1, num2):
 sg.theme('DarkBrown')
 
 layout = [[sg.Text('-----Add APP-----')],
-        [sg.Text('Enter your 1st number:'),sg.InputText()],
-        [sg.Text('Enter your 2nd number:'),sg.InputText()],
+        [sg.Text('Enter your 1st number:'),sg.InputText(size=(20,1),key='in_1')],
+        [sg.Text('Enter your 2nd number:'),sg.InputText(size=(20,1), key='in_2')],
         [sg.Text('Output:')],
         [sg.Text('Output Here', key='-OUT-')],
-        [sg.Button('Add'), sg.Button('Exit')]]
+        [sg.Button('Add'),sg.Button('Clear'), sg.Button('Exit')]]
 
 window = sg.Window('Add Two Number', layout)
 
@@ -19,10 +19,16 @@ while True:
     if event == 'Exit' or event == sg.WIN_CLOSED:
         break
     if event == 'Add':
-        num1 = int(values[0])
-        num2 = int(values[1])
+        num1 = int(values['in_1'])
+        num2 = int(values['in_2'])
         result = addFunc(num1, num2)
         window['-OUT-'].update(result)
+
+    if event == 'Clear':
+        window['in_1'].update('')
+        window['in_2'].update('')
+        window['-OUT-'].update('Output Here')
+        
     
 
 window.close()
